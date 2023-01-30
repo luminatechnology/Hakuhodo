2af4cb66a8818026cb11207307b5dc35764c22d4
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2023-01-30 10:51:10 +0800

    Add new field.

--- eGUICustomizationsByBranch/eGUICustomizationsByBranch/DAC_Extension/TaxZoneExtensions.cs
+++ eGUICustomizationsByBranch/eGUICustomizationsByBranch/DAC_Extension/TaxZoneExtensions.cs
@@ -11,5 +11,13 @@ namespace PX.Objects.TX
         public virtual bool? UsrWHTTaxRelated { get; set; }
         public abstract class usrWHTTaxRelated : PX.Data.BQL.BqlBool.Field<usrWHTTaxRelated> { }
         #endregion
+
+        #region UsrNHITaxRelated
+        [PXDBBool]
+        [PXUIField(DisplayName = "NHI Tax Related")]
+        [PXDefault(false, PersistingCheck = PXPersistingCheck.Nothing)]
+        public virtual bool? UsrNHITaxRelated { get; set; }
+        public abstract class usrNHITaxRelated : PX.Data.BQL.BqlBool.Field<usrNHITaxRelated> { }
+        #endregion
     }
 }
