81292d160ff7076ec14456d561ff2acb785c09d5
Alton.lee <woot2130@gmail.com>
2023-02-20 17:18:56 +0800

    專案請款產生的專案交易，統一為應稅

---
+++ UCG_Customization/UCG_Customization/Graph_Extension/PMBillEngineUCGExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using PX.Data;
+using PX.Objects.AR;
+
+namespace PX.Objects.PM
+{
+    public class PMBillEngineUCGExt : PXGraphExtension<PMBillEngine>
+    {
+        #region Override
+        public delegate PMProformaLine InsertTransactionDelegate(PMProject project, PMProformaLine tran, string subCD, string note, Guid[] files);
+        [PXOverride]
+        public virtual PMProformaLine InsertTransaction(PMProject project, PMProformaLine tran, string subCD, string note, Guid[] files, InsertTransactionDelegate baseMethod)
+        {
+            var returnValue = baseMethod(project, tran, subCD, note, files);
+
+            var tax = TX.TaxCategory.PK.Find(Base, "TAXABLE");
+            if (tax != null) returnValue.TaxCategoryID = tax.TaxCategoryID;
+
+            return returnValue;
+        }
+        #endregion
+    }
+}
