20756c46a465372e9cdcade580530e111743ab95
Alton.lee <woot2130@gmail.com>
2023-02-20 10:25:38 +0800

    起多張單檢核移除

---
+++ UCG_Customization/UCG_Customization/Graph_Extension/ProformaEntryUCGExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PX.Data;
+
+namespace PX.Objects.PM
+{
+    public class ProformaEntryUCGExt : PXGraphExtension<ProformaEntry>
+    {
+        #region Override
+        public delegate void ValidatePrecedingBeforeReleaseDelegate(PMProforma doc);
+        [PXOverride]
+        public virtual void ValidatePrecedingBeforeRelease(PMProforma doc, ValidatePrecedingBeforeReleaseDelegate baseMethod)
+        {
+        }
+
+        public delegate void ValidatePrecedingInvoicesBeforeReleaseDelegate(PMProforma doc);
+        [PXOverride]
+        public virtual void ValidatePrecedingInvoicesBeforeRelease(PMProforma doc, ValidatePrecedingInvoicesBeforeReleaseDelegate baseMethod)
+        {
+        }
+        #endregion
+    }
+}
