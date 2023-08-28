3a7e1d67196121ec908201fff624d97ba5c61e9b
Alton.lee <woot2130@gmail.com>
2023-08-28 17:23:03 +0800

    PO產生預付款，自動帶專案

---
+++ UCG_Customization/UCG_Customization/Graph_Extension/APInvoiceEntryExt/PreaymentsUCGExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.AP;
+using PX.Objects.CT;
+
+namespace PX.Objects.PO.GraphExtensions.APInvoiceEntryExt
+{
+    public class PreaymentsUCGExt : PXGraphExtension<Prepayments, APInvoiceEntry>
+    {
+        #region Override
+        public delegate void AddPOOrderProcDelegate(POOrder order, bool createNew);
+
+        [PXOverride]
+        public virtual void AddPOOrderProc(POOrder order, bool createNew, AddPOOrderProcDelegate baseMethod)
+        {
+            baseMethod(order, createNew);
+            var doc = Base.Document.Current;
+            var contract = Contract.PK.Find(Base, order.ProjectID);
+            Base.Document.Cache.SetValueExt(doc, APInvoiceEntry_WorkGroupExtension.UD_PROJECT, contract?.ContractCD);
+        }
+        #endregion
+    }
+}
