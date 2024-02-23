f5d5eb9c8863a14ae23b6fec71fc7c96ebc8e2f5
Jeff <jeff.liang@lumina-tech.com>
2024-02-23 16:05:42 +0800

    Since the static method [IsActive] will be initialized when the session is established, reset cache to make the method fresh.

---
+++ UCGLedgerSettlement/UCGLedgerSettlement/Graph_Extensions/GLSetpMaint.cs
@@ -0,0 +1,24 @@
+﻿using PX.Data;
+
+namespace PX.Objects.GL
+{
+    public class GLSetupMaint_Extensions : PXGraphExtension<GLSetupMaint>
+    {
+        #region Delegate Methods
+        public delegate void PersistDelegate();
+        [PXOverride]
+        public void Persist(PersistDelegate baseMethod)
+        {
+            var orig = (bool)Base.GLSetupRecord.Cache.GetValueOriginal<GLSetupExt.usrEnblSettlingLedgerCmplProj>(Base.GLSetupRecord.Current);
+            var curr = (bool)Base.GLSetupRecord.Cache.GetValue<GLSetupExt.usrEnblSettlingLedgerCmplProj>(Base.GLSetupRecord.Current);
+
+            baseMethod();
+
+            if (orig != curr)
+            {
+                PXGraph.CreateInstance<PX.SM.UpdateMaint>().ResetCachesCommand.Press();
+            }
+        }
+        #endregion
+    }
+}
