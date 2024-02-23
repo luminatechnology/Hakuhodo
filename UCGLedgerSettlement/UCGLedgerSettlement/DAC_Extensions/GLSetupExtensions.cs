0fd697dbb9046ca85f674e67dec2112156bd562f
Jeff <jeff.liang@lumina-tech.com>
2024-02-23 16:02:56 +0800

    Added new control field.

---
+++ UCGLedgerSettlement/UCGLedgerSettlement/DAC_Extensions/GLSetupExtensions.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+
+namespace PX.Objects.GL
+{
+    public class GLSetupExt : PXCacheExtension<PX.Objects.GL.GLSetup>
+    {
+        #region UsrEnblSettlingLedgerCmplProj
+        [PXDBBool]
+        [PXUIField(DisplayName = "Enable Settling Ledger On Completed Projects")]
+        public virtual bool? UsrEnblSettlingLedgerCmplProj { get; set; }
+        public abstract class usrEnblSettlingLedgerCmplProj : PX.Data.BQL.BqlBool.Field<usrEnblSettlingLedgerCmplProj> { }
+        #endregion
+    }
+}
